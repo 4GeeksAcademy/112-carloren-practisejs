c615b3e3f65e5bb4a5576917c9bff323d8062be9
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-21 09:05:55 +0000

    deshacer cambios en rama main

--- src/app.js
+++ src/app.js
@@ -6,22 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  const devices = ["teclado", "mouse", "webcam", "auriculares"]
-
-  console.log(devices);
-
-  const lista = document.getElementById("items-list")
-
-  const elementos = devices.map(device => {
-
-    const li = document.createElement('li')
-    li.textContent = device
-    li.classList.add('list-group-item')
-    lista.append(li) //append inserta texto o elementos en el elemento HTML guardado en la variable, en este caso el ul
-
-    //return li
-  })
-  // con spread operator (...) tendriamos que tener return en la funcion
-  //lista.append(...elementos)
-
+  
+  
 };
