e19238b54bd74c709306a5ebd8e05c5fc8fa167b
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-21 09:03:22 +0000

    crear li desde un array en js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const devices = ["teclado", "mouse", "webcam", "auriculares"]
+
+  console.log(devices);
+
+  const lista = document.getElementById("items-list")
+
+  const elementos = devices.map(device => {
+
+    const li = document.createElement('li')
+    li.textContent = device
+    li.classList.add('list-group-item')
+    lista.append(li) //append inserta texto o elementos en el elemento HTML guardado en la variable, en este caso el ul
+
+    //return li
+  })
+  // con spread operator (...) tendriamos que tener return en la funcion
+  //lista.append(...elementos)
+
 };
