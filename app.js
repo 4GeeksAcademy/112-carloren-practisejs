b09053f8d724f14f74c2bc3709aa97f30d810a25
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-21 10:36:53 +0000

    A medias

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  
-  
+  const productos = [
+    { nombre: 'Mouse', precio: 25 },
+    { nombre: 'Teclado', precio: 45 },
+    { nombre: 'Monitor', precio: 150 },
+
+  ]
+
+  const container = document.getElementById('card-container')
+
+  productos.map(producto => {
+    const div = document.createElement('div')
+    div.className = "col-md-4"
+
+    div.innerHTML = `
+    <div class="card h-100">
+      <div>
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.precio}</p>
+      </div>
+    </div>
+    `
+  })
+
 };
