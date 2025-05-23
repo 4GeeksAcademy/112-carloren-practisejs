b57f650818bd18cd7cd185f6c6029b8a5403ec56
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-23 08:33:48 +0000

    convertir for en map y map anidado

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+const numbers = [1, 2, 3]
+
+// const response = []
+
+// for (let i = 0; i < numbers.length; i++) {      //Esto sería con un for
+//     response.push(numbers[i] * 2)
+// }
+
+const response = numbers.map(num => num * 2)      //Lo mismo que el for pero en mucho menos código
+
+console.log(response);
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const matriz = [
+    [1, 2],                        //Queremos que los números de cada fila queden con +1
+    [3, 4]
+]
 
-window.onload = function () {
-  
-  
-};
+const masUno = matriz.map(fila => fila.map(num => num + 1))
+                                                                //Con un map anidado podemos recorrer los array (fila) dentro del array matriz
+console.log(masUno);
