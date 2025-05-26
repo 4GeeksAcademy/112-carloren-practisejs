3349869fac1d49a4211f34767f6443c5562692c9
Carlos Lorenzo <carloren.1996@gmail.com>
2025-05-26 10:08:44 +0000

    repaso 26/05

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
-import "./style.css";
+
+// Bucle for ----------------------
+
+// let vueltas = parseInt(5)
+
+// for (let vuelta = 1; vuelta <= vueltas; vuelta++) {
+//     console.log(`vuelta ${vuelta}`);
+
+// }
+
+// console.log(`Has completado las ${vueltas} vueltas`);
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Bucle for en un array -----------------------------
 
-window.onload = function () {
-  
-  
-};
+// let frutas = ["mango", "fresa", "plátano"]
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+
+// }
+
+
+// Tratamiento de un objeto --------------------------------
+
+let persona = {
+    nombre: "Carlos",
+    apellido: "Lorenzo"
+}
+
+// console.log(persona.nombre);   // persona.nombre es una dot notation
+console.log(persona["nombre"]);  // persona["nombre"] es una bracket notation, equivalente
